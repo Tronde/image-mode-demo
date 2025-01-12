464722bebddb27ef4f46d470c12d4fa015148adc
Joerg Kastning <joerg@redhat.com>
2025-01-12 21:10:09 +0100

    Add commands to remove firewall rules
    Signed-off-by: Joerg Kastning <joerg@redhat.com>

--- destroy_simple_container_registry.sh
+++ destroy_simple_container_registry.sh
@@ -23,3 +23,8 @@ podman rm ${registry_name}
 podman rmi registry:latest
 rm /etc/pki/ca-trust/source/anchors/${cert_domain}.crt
 update-ca-trust
+
+# Configure the firewall
+firewall-cmd --remove-port=5000/tcp --zone=internal --permanent
+firewall-cmd --remove-port=5000/tcp --zone=public --permanent
+firewall-cmd --reload
